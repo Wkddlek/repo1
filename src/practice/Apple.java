ddfd74ba982a6b6f53cafa9e5ed4183f31c5e8d8
Gi7D-01 <Gi7D-01@DESKTOP-ETC2N35>
2022-06-14 14:51:15 +0900

    Apple에 Float 추가

--- src/practice/Apple.java
+++ src/practice/Apple.java
@@ -1,5 +1,17 @@
 package practice;
 
 public class Apple {
+	int a;
+	int b;
+	int c;
+	Float d;
+	
+	public Apple(int a, int b, int c, Float d) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = 0.123F;
+		
+	}
 
 }
